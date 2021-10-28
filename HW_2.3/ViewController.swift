59bf92c046d857a98a20067072a31a4e7fc9b5f2
Ilya Pokhodin <ilya.pokhodin@gmail.com>
2021-10-28 10:55:25 +0300

    Initial Commit

---
+++ HW_2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.3
+//
+//  Created by Ilya Pokhodin on 28.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
