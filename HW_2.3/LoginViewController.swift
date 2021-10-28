a81d91afb0b3e3768d593b988284c9932249b4db
Ilya Pokhodin <ilya.pokhodin@gmail.com>
2021-10-28 14:00:55 +0300

    Вся логика реализована, осталось подточить код и реализовать методы с клавиатурой

---
+++ HW_2.3/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  HW_2.3
+//
+//  Created by Ilya Pokhodin on 28.10.2021.
+//
+
+import UIKit
+
+    class LoginViewController: UIViewController {
+
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    private let userName = "tolstovec"
+    private let password = "tolstovec1"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func fogotUserNameTouchButton() {
+        showAlert(title: "Забыли логин?", messege: "Ваш логин \(userName)")
+    }
+    
+    @IBAction func fogotPasswordTouchButton() {
+        showAlert(title: "Забыли пароль?", messege: "Ваш пароль \(password)")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if userNameTextField.text == userName
+            && passwordTextField.text == password {
+            
+            guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+            welcomeVC.welcomeLableValue = "Welcome, " + userName
+        } else {
+             showAlert(title: "Логин или пароль неверны", messege: "Укажите корректные логин и пароль")
+            passwordTextField.text = ""
+        }
+    
+    }
+    
+        @IBAction func unwind(for segue: UIStoryboardSegue) {
+            userNameTextField.text = ""
+            passwordTextField.text = ""
+        }
+}
+
+extension LoginViewController {
+    private func showAlert(title: String, messege: String) {
+        let alertLogIn = UIAlertController(title: title, message: messege, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alertLogIn.addAction(okAction)
+        present(alertLogIn, animated: true)
+    }
+}
---
+++ HW_2.3/WelcomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeViewController.swift
+//  HW_2.3
+//
+//  Created by Ilya Pokhodin on 28.10.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomUserNameLable: UILabel!
+    
+    var welcomeLableValue: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        welcomUserNameLable.text = welcomeLableValue
+    }
+    
+}
